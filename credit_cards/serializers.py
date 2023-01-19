7b2542861c31bb0c211212139724027953ee415f
MrMika96 <mikamc1996@gmail.com>
2023-01-19 16:06:17 +0300

    Small field changes

--- credit_cards/serializers.py
+++ credit_cards/serializers.py
@@ -4,9 +4,14 @@ from credit_cards.models import CreditCard
 
 
 class CreditCardSerializer(serializers.ModelSerializer):
+    owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = CreditCard
         fields = [
             'id', 'owner', 'number',
             'expiration_date', 'cvv', 'bank', 'payment_system'
+        ]
+        read_only_fields = [
+            'bank', 'payment_system'
         ]
