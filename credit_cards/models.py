ddb880cb1f67d33a6d205e62b313d7f442c75ece
MrMika96 <mikamc1996@gmail.com>
2023-01-23 09:24:13 +0300

    Changes for credit card model

--- credit_cards/models.py
+++ credit_cards/models.py
@@ -5,12 +5,20 @@ from users.models import User
 
 
 class CreditCard(models.Model):
+    CURRENCY_CHOICES = [
+        ('USD', 'USD'),
+        ('EUR', 'EUR'),
+        ('JPY', 'JPY'),
+        ('RUB', 'RUB')
+    ]
     owner = models.ForeignKey(User, related_name="credit_cards", on_delete=models.CASCADE)
     number = models.CharField(max_length=19, blank=False, null=False)
     expiration_date = models.DateField(blank=False, null=False)
     cvv = models.IntegerField()
     bank = models.ForeignKey(Bank, related_name="cards", on_delete=models.CASCADE)
     payment_system = models.ForeignKey(PaymentSystem, related_name="cards", on_delete=models.CASCADE)
+    currency = models.CharField(max_length=16, choices=CURRENCY_CHOICES, null=True, default=None)
+    balance = models.FloatField(default=0, null=False, blank=False)
 
     class Meta:
         db_table = "credit_cards"
