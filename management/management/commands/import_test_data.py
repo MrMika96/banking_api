2a337515f63b59a98a40c0ddddbe0f95b8ccb1ad
MrMika96 <mikamc1996@gmail.com>
2023-01-31 10:13:53 +0300

    Added new management command for loading test data from fixtures

--- management/management/commands/import_test_data.py
+++ management/management/commands/import_test_data.py
@@ -1,18 +1,21 @@
 from django.apps import apps
 from django.core.management import call_command
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.db.transaction import atomic
 
 
 class Command(BaseCommand):
-    help = 'Add test data in database'
+    help = 'Adding test data to the database'
 
     @atomic
     def handle(self, *args, **options):
         for app_name in apps.get_app_configs():
             try:
-                call_command('loaddata', f"""{str(app_name.verbose_name).lower()}/fixtures/initial_data.json""")
-            except:
+                call_command(
+                    'loaddata',
+                    f"""{str(app_name.verbose_name).lower()}/fixtures/initial_data.json""",
+                )
+            except CommandError:
                 continue
         self.stdout.write(self.style.SUCCESS("Complete!"))
 
