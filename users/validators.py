5e2db9d33841f18c4cb6b49cefe6a9ac7745bda4
MrMika96 <mikamc1996@gmail.com>
2026-05-25 14:28:14 +0300

    feat: removed app 'images'

--- users/validators.py
+++ users/validators.py
@@ -6,13 +6,10 @@ from PIL import Image as PILImage
 
 def validate_profile_image(value):
     """Validate image type and size."""
-    # Проверка размера (максимум 500KB)
     if value.size > 500 * 1024:
         raise ValidationError(
             f"Image size too large. Max 500KB, got {value.size // 1024}KB"
         )
-
-    # Проверка типа через PIL
     try:
         img = PILImage.open(value)
         if img.format not in ["JPEG", "PNG"]:
